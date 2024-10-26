e82509084b6582c6b38c8532a94c540737ec4b1d
Ian <ian@crockwellsolutions.com>
2024-10-26 20:34:59 +0100

    AWS SAM Local Debugging Setup Complete

---
+++ lib/stateless/stateless-stack.ts
@@ -0,0 +1,23 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CustomLambda } from '../constructs/custom-lambda';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+
+export class StatelessStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Create a queue that will be used to receive messages from the Lambda function
+    const debuggingQueue = new Queue(this, 'LambdaDebuggerQueue', {
+      queueName: 'LambdaDebuggerQueue',
+    });
+
+    // Create a Lambda function that will send messages to the queue and demonstrate debugging
+    const debuggingFunction = new CustomLambda(this, 'LambdaDebuggingFunction', {
+      path: 'src/lambda-debugging-function.ts',
+    }).lambda;
+
+    // Grant the Lambda function permissions to send messages to the queue
+    debuggingQueue.grantSendMessages(debuggingFunction);
+  }
+}
---
+++ src/lambda-debugging-function.ts
@@ -0,0 +1,49 @@
+/**
+ * Lambda Debugger Function
+ * 
+ * A simple test function that performs the following actions
+ * - Logs the event object passed to the function
+ * - Sends the event payload to an SQS queue
+ * - Waits for a random amount of time between 1 and 10 seconds
+ * - Logs the message ID of the message sent to the SQS queue
+ * 
+ * This function is used to demonstrate how to debug a Lambda function using the AWS CDK
+ * and the Serverless Framework.
+ */
+
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+const SQS_QUEUE_URL = process.env.SQS_QUEUE_URL;
+
+// Use the AWS Lambda Powertools Logger
+import { Logger } from '@aws-lambda-powertools/logger';
+export const logger = new Logger();
+
+// Setup the sqs client to send messages to the queue
+const sqsClient = new SQSClient({ region: 'eu-west-1' });
+
+/**
+ * Lambda Handler
+ *
+ * @param {object} event - The event object containing the payload passed to this function.
+ * @param {object} context - The context object provided by the AWS Lambda runtime.
+ */
+export async function handler(event: any) {
+  logger.info('Processing lambda debugger function', { event: event });
+
+  logger.info('Sending message to SQS queue', { queueUrl: SQS_QUEUE_URL });
+
+  const params = new SendMessageCommand({
+    QueueUrl: SQS_QUEUE_URL,
+    MessageBody: JSON.stringify(event),
+  });
+
+  try {
+    const response = await sqsClient.send(params);
+    logger.info('Response from queue', { response: response });
+  } catch (err) {
+    logger.error('Error invoking queue', { error: err });
+  }
+
+  logger.info('Lambda debugger function completed');
+
+};
